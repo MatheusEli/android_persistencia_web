cd67c5c39ee95133b81943e9feb52a31b91cc77c
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-07-31 07:04:38 -0300

    Implementando permissoes de rede e Gson Converter

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -2,6 +2,7 @@ package br.com.alura.estoque.retrofit;
 
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
@@ -9,7 +10,10 @@ public class EstoqueRetrofit {
 
     public EstoqueRetrofit() {
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://localhost:8080/").build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.15.21:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
         produtoService = retrofit.create(ProdutoService.class);
     }
 
