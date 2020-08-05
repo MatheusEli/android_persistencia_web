1ce61dfd0bbb18b69c7ee2e20e9b43a5d69be398
MatheusEli <matheuselisouzaferreira@hotmail.com>
2020-08-05 06:54:41 -0300

    Refatorando código

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -47,35 +47,34 @@ public class ListaProdutosActivity extends AppCompatActivity {
     }
 
     private void buscaProdutos() {
+        buscaProdutosInternos();
+    }
+
+    private void buscaProdutosInternos() {
+        new BaseAsyncTask<>(dao::buscaTodos,
+                resultado -> {
+                    adapter.atualiza(resultado);
+                    buscaProdutosNaApi();
+                }).execute();
+    }
+
+    private void buscaProdutosNaApi() {
 
         ProdutoService service = new EstoqueRetrofit().getProdutoService();
         Call<List<Produto>> call = service.buscaTodos();
 
-        new BaseAsyncTask<>(dao::buscaTodos,
-                resultado -> {
-                    adapter.atualiza(resultado);
-                    new BaseAsyncTask<>(() -> {
-                        try {
-                            Thread.sleep(3000);
-                            Response<List<Produto>> resposta = call.execute();
-                            List<Produto> produtosNovos = resposta.body();
-                            dao.salva(produtosNovos);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return dao.buscaTodos();
-                    }, produtosNovos -> {
-                        if(produtosNovos != null){
-                            adapter.atualiza(produtosNovos);
-                        } else {
-                            Toast.makeText(this,
-                                    "Não possível buscar os produtos da API",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-                }).execute();
+        new BaseAsyncTask<>(() -> {
+            try {
+                Response<List<Produto>> resposta = call.execute();
+                List<Produto> produtosNovos = resposta.body();
+                dao.salva(produtosNovos);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return dao.buscaTodos();
+        }, produtosNovos ->
+                adapter.atualiza(produtosNovos))
+            .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     private void configuraListaProdutos() {
